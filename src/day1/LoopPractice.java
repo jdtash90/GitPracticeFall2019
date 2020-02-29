28a14244f34567bf540f46686da02abc9cab77ac
JahongirD <jahon15206@gmail.com>
2020-02-29 15:54:48 -0500

    nester forloop added

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -6,7 +6,10 @@ public class LoopPractice {
         // random code
         for (int x = 0; x <10 ; x+=2) {
             System.out.println("x = " + x);
-            
+            for (int i = 0; i <10 ; i++) {
+                System.out.println("i = " + i);
+                
+            }
         }
     }
 }
