d0400604e44a81e731498a5360f3e8543dae8d25
JahongirD <jahon15206@gmail.com>
2020-02-29 15:41:50 -0500

    Added new loop practice

---
+++ src/day1/LoopPractice.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class LoopPractice {
+    public static void main(String[] args) {
+
+        // random code
+        for (int x = 0; x <10 ; x+=2) {
+            System.out.println("x = " + x);
+            
+        }
+    }
+}
